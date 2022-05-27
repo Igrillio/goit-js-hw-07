ae0fc02a0386b830648584585c9d920873262c58
Igor <99814263+Igrillio@users.noreply.github.com>
2022-05-27 10:02:37 +0300

    д

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,34 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+let instance 
+const list = document.querySelector(".gallery");
+
+function markup() {
+return galleryItems.map((imageAdd) => `<div class="gallery__item">
+  <a class="gallery__link" href=${imageAdd.original}>
+    <img
+      class="gallery__image"
+      src ="${imageAdd.preview}"
+      data-source ="${imageAdd.original}"
+      alt ="${imageAdd.description}"
+      />
+  </a>
+</div>`)
+  .join("");
+}
+list.insertAdjacentHTML("afterbegin", markup(galleryItems));
 
 console.log(galleryItems);
+list.addEventListener('click', onImgClick);
+
+function onImgClick(event) {
+  event.preventDefault();
+
+  if (event.target.nodeName !== 'IMG') {
+    return;
+  }
+  instance = basicLightbox.create(`<img src="${event.target.dataset.source}">`);
+
+  instance.show();
+}
+
